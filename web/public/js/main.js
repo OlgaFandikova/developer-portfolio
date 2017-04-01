ef1dfdc3ff7aef0e1e861de23b9267f94787caf7
Olga Fandikova <olafan92@gmail.com>
2017-04-01 23:43:13 +0700

    скролл по якорю

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,30 +1,85 @@
-var Initialize = {
-    scrollToFixed: function() {
-        $('.navbar-scrolltofixed').scrollToFixed();
-    },
-    wow: function() {
-        var wow = new WOW({
-            mobile: false
-        });
-        wow.init();
-    },
-    parallax: function() {
-        $(window).stellar();
-    }
-};
-
-var Togglers = {
-    header: function () {
-        $('.toogle-nav').on('click', function() {
-            $('.responsive-menu, .navbar').toggleClass('open');
-        })
-    }
-};
-
 $(document).ready(function() {
+
+    var Initialize = {
+
+        scrollToFixed: function() {
+            $('.navbar-scrolltofixed').scrollToFixed();
+        },
+
+        wow: function() {
+            var wow = new WOW({
+                mobile: false
+            });
+            wow.init();
+        },
+
+        parallax: function() {
+            $(window).stellar();
+        }
+    };
+
+    var Togglers = {
+
+        header: function () {
+            $('.toogle-nav').on('click', function() {
+                $('.responsive-menu, .navbar').toggleClass('open');
+            })
+        }
+    };
+
+    var $navbarItem = $('.nav-menu a');
+
+    var Navbar = {
+
+        init: function() {
+            $(document).on("scroll", this.onScroll);
+        },
+
+        onScroll: function() {
+            var scrollPos = $(document).scrollTop();
+
+            $navbarItem.each(function () {
+                var currLink = $(this);
+                var refElement = $(currLink.attr("href"));
+                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                    $navbarItem.removeClass("active");
+                    currLink.addClass("active");
+                }
+                else{
+                    currLink.removeClass("active");
+                }
+            });
+        },
+
+        click: function() {
+            $navbarItem.on('click', function (e) {
+                e.preventDefault();
+                $(document).off("scroll");
+
+                $('a').each(function () {
+                    $(this).removeClass('active');
+                });
+                $(this).addClass('active');
+
+                var target = this.hash,
+                    $target = $(target);
+
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top+2
+                }, 500, 'swing', function () {
+                    window.location.hash = target;
+                    $(document).on("scroll", this.onScroll);
+                });
+            });
+        }
+    };
+
     Initialize.scrollToFixed();
     Initialize.wow();
     Initialize.parallax();
 
     Togglers.header();
-})
+
+    Navbar.init();
+    Navbar.click();
+});
