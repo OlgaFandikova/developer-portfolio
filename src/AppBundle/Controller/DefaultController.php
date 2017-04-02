2aa303a7bbc0620ac645214edbcfa5eb25f957a1
Olga Fandikova <olafan92@gmail.com>
2017-04-02 19:02:42 +0700

    пополняю портфолио

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -23,4 +23,36 @@ class DefaultController extends Controller
     {
         return $this->render('homepage/portfolio/arctic.html.twig');
     }
+
+    /**
+     * @Route("/portfolio/lunacharsky", name="lunacharsky")
+     */
+    public function portfolioLunacharskyAction()
+    {
+        return $this->render('homepage/portfolio/lunacharsky.html.twig');
+    }
+
+    /**
+     * @Route("/portfolio/mircar", name="mircar")
+     */
+    public function portfolioMircarAction()
+    {
+        return $this->render('homepage/portfolio/mircar.html.twig');
+    }
+
+    /**
+     * @Route("/portfolio/prava70", name="prava70")
+     */
+    public function portfolioPrava70Action()
+    {
+        return $this->render('homepage/portfolio/prava70.html.twig');
+    }
+
+    /**
+     * @Route("/portfolio/chemistry", name="chemistry")
+     */
+    public function portfolioСhemistryAction()
+    {
+        return $this->render('homepage/portfolio/chemistry.html.twig');
+    }
 }
